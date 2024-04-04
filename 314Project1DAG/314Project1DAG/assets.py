09734e87a844762437c4d7379413bf703b9a2fe7
Elizabeth Girling <71190947+egirling@users.noreply.github.com>
2024-04-04 18:15:09 -0500

    First asset added

--- 314Project1DAG/314Project1DAG/assets.py
+++ 314Project1DAG/314Project1DAG/assets.py
@@ -1,1 +1,11 @@
+import json
+import os
 
+from dagster import asset # import the `dagster` library
+
+@asset # add the asset decorator to tell Dagster this is an asset
+def training_data() -> None:
+
+    os.makedirs("trainting_data", exist_ok=True)
+    with open("spaceship-titanic/train.csv", "w") as f:
+        json.dump(training_data, f)
