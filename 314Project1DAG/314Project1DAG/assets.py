cd7908a6a0e3145754e66fbc3011a10dd96868e9
Elizabeth Girling <71190947+egirling@users.noreply.github.com>
2024-04-04 18:55:23 -0500

    First asset materializes

--- 314Project1DAG/314Project1DAG/assets.py
+++ 314Project1DAG/314Project1DAG/assets.py
@@ -1,11 +1,9 @@
-import json
-import os
+import pandas as pd
 
 from dagster import asset # import the `dagster` library
 
 @asset # add the asset decorator to tell Dagster this is an asset
 def training_data() -> None:
-
-    os.makedirs("trainting_data", exist_ok=True)
-    with open("spaceship-titanic/train.csv", "w") as f:
-        json.dump(training_data, f)
+    data = pd.read_csv("spaceship-titanic/train.csv")
+    #return data
+   
