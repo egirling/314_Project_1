cc160711b02dcde3753e990979be7156b65c396a
Elizabeth Girling <71190947+egirling@users.noreply.github.com>
2024-04-04 22:48:57 -0500

    First asset actually working

--- 314Project1DAG/314Project1DAG/assets.py
+++ 314Project1DAG/314Project1DAG/assets.py
@@ -1,9 +1,27 @@
 import pandas as pd
 
-from dagster import asset # import the `dagster` library
+from dagster import MetadataValue, asset, MaterializeResult, AssetExecutionContext # import the `dagster` library
+data = pd.read_csv("spaceship-titanic/train.csv")
 
 @asset # add the asset decorator to tell Dagster this is an asset
-def training_data() -> None:
-    data = pd.read_csv("spaceship-titanic/train.csv")
-    #return data
-   
+def splitWomanAndChildrenFromMen(context: AssetExecutionContext) -> MaterializeResult:
+    adults = data[data['Age'] > 18]
+    children = data[data['Age'] <= 18]
+
+    total_adults = len(adults)
+    total_children = len(children)
+
+    survived_adults = (adults['Transported'] == False).sum()  
+    survived_children = (children['Transported'] == False).sum()  
+
+    survival_rate_adults = float(survived_adults / total_adults if total_adults > 0 else 0)
+    survival_rate_children = float(survived_children / total_children if total_children > 0 else 0)
+    
+    return MaterializeResult(
+        metadata={
+            "Survival Rate of Adults": survival_rate_adults, 
+            "Survival Rate of Children": survival_rate_children,
+        }
+    )
+    
+
